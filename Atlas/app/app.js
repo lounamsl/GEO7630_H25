bcd2b9bf4fc910c10572e1d3770490700ccea1ac
lounamsl <lounamasliah@gmail.com>
2025-03-18 19:42:20 +0000

    lab10

--- Atlas/app/app.js
+++ Atlas/app/app.js
@@ -7,80 +7,68 @@ var map = new maplibregl.Map({
     hash: true // activation du hash pour la gestion de l'historique de la carte
 });
 
-
-function loadTeam(teamName) {
-    // Vider toutes les divs
-    document.getElementById('Equipe1').innerHTML = '';
-    document.getElementById('Equipe2').innerHTML = '';
-    document.getElementById('Equipe3').innerHTML = '';
-    document.getElementById('Equipe4').innerHTML = '';
-    document.getElementById('Equipe5').innerHTML = '';
-    document.getElementById('Equipe6').innerHTML = '';
-    document.getElementById('Equipe7').innerHTML = '';
-    // Charger le contenu de l'équipe sélectionnée
-    fetch('./' + teamName + '/index.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById(teamName).innerHTML = data;
-            // Charger les fichiers .js depuis le répertoire spécifique
-            const scripts = document.getElementById(teamName).querySelectorAll('script[src]');
-            scripts.forEach(script => {
-                const newScript = document.createElement('script');
-                newScript.src = `./${teamName}/${script.getAttribute('src')}`;
-                document.body.appendChild(newScript);
-            });
-           // Charger les fichiers .css depuis le répertoire spécifique
-            const links = document.getElementById(teamName).querySelectorAll('link[rel="stylesheet"]');
-            links.forEach(link => {
-                const newLink = document.createElement('link');
-                newLink.rel = 'stylesheet';
-                newLink.type = 'text/css';  // Ajoutez ce type pour éviter l'erreur de type MIME
-                newLink.href = `./${teamName}/${link.getAttribute('href')}`;
-                document.head.appendChild(newLink);
-            });
-        })
-        .catch(error => {
-            console.error('There was a problem with fetching the team content:', error);
-        });
+var nav = new maplibregl.NavigationControl({
+    showCompass: true,
+    showZoom: true,
+    visualizePitch: true
+});
+map.addControl(nav, 'top-right');
+var geolocateControl = new maplibregl.GeolocateControl({
+    positionOptions: { enableHighAccuracy: true },
+    trackUserLocation: true
+});
+map.addControl(geolocateControl, 'bottom-right');
+var scale = new maplibregl.ScaleControl({ unit: 'metric' });
+map.addControl(scale);
+map.on('load', function () {
+    map.addSource('densite_arbres_quartiers_source', {
+        type: 'vector',
+        tiles: ['https://silver-parakeet-g4rr6w7gr5gj2w57p-8801.app.github.dev/public.densite_arbres_quartiers/{z}/{x}/{y}.pbf']
+    });
+    map.addLayer({
+        'id': 'densite_arbres_quartiers',
+        'type': 'fill',
+        'source': 'densite_arbres_quartiers_source',
+        'source-layer': 'public.densite_arbres_quartiers',
+        'paint': {
+   'fill-color': [
+        'interpolate',
+        ['linear'],
+        ['get', 'qt_arbres'],
+        0, 'rgb(255, 255, 255)',
+        100, 'rgb(28, 212, 245)',
+        6041, 'rgb(0, 0, 255)'
+    ],
+    'fill-opacity': 0.7
 }
 
-
-
-const myLayers = ['grid', 'buffer', 'rdp', 'union', 'joined']
-
-// Cette fonction est appelée lorsque la carte est chargée.
-map.on('load', function () {
-    // Charge une couche de points aléatoires.
-    loadRandomPointsLayer()
-    // Ajoute un contrôle de légende personnalisé.
-    // Lab 12
-    // map.addControl(new MaplibreLegendControl({ 
-    //     rdp: "rdp", 
-    //     grid: 'grid',
-    //     union: 'union',
-    //     buffer: 'buffer',
-    //     joined: 'joined'
-    // }, { onlyRendered: true }), "bottom-left");
-});
-
-// Cette fonction est appelée lorsque la carte est en attente.
-map.on('idle', function () {
-    // Récupère les couches de style de la carte.
-    const layers = map.getStyle().layers;
-    // Parcourt toutes les couches.
-    layers.forEach((layer) => {
-        // Si la couche est 'joined', met à jour le compteur dynamique.
-        if (layer.id == 'joined') {
-            dynamicCount()
-        // Sinon, si la couche est 'rdp', met à jour le compteur de fonctionnalités.
-        } else if (layer.id == 'rdp') {
-            featureCount()
-        }        
+ 
     });
-    // alert('test')
 });
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function loadWFS() {
+    map.addSource('arrondissements-source', {
+        type: 'geojson',
+        data: 'https://silver-parakeet-g4rr6w7gr5gj2w57p-9000.app.github.dev/collections/MASL68310301.arrondissements/items.json?limit=10000'
+    });
+    map.addLayer({
+        'id': 'arrondissements',
+        'type': 'fill',
+        'source': 'arrondissements-source',
+        'paint': {
+            'fill-outline-color': 'black',
+            'fill-color': getRandomColor(),
+            'fill-opacity': 0.3
+        } 
+    });
+    
+
+}
